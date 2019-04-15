0a0dd55170e8178770aed52ed7cb9164397a2c01
yu mitsuhori <ymbullseye@gmail.com>
2019-04-16 00:09:25 +0900

    不要なファイル削除

--- app/src/main/java/jp/co/youmitsu/myapplication/MainActivity.kt
+++ app/src/main/java/jp/co/youmitsu/myapplication/MainActivity.kt
@@ -6,34 +6,8 @@ import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    var text: String = ""
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test)
-//        nickname_value.setOnTouchListener { v, event ->
-//            motion_layout.transitionToEnd()
-//            edit_text.requestFocus()
-//            edit_text.setSelection(edit_text.length())
-//            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-//            imm.showSoftInput(edit_text, InputMethodManager.SHOW_IMPLICIT)
-//            true
-//        }
-//        edit_text.setOnFocusChangeListener { v, hasFocus ->
-//            text = (edit_text.text as SpannableStringBuilder).toString()
-//            edit_text.setText(text)
-//            nickname_value.text = if (text.isEmpty()) "--" else text
-//            if (!hasFocus) {
-//                motion_layout.transitionToStart()
-//                val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-//                inputMethodManager.hideSoftInputFromWindow(motion_layout.windowToken, 0)
-//            }
-//        }
-//        motion_layout.setOnTouchListener { v, event ->
-//            edit_text.clearFocus()
-//            true
-//        }
-//        nickname_value.text = if (text.isEmpty()) "--" else text
-//        edit_text.setText(text)
+        setContentView(R.layout.activity_main)
     }
 }
